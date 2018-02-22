7ab6258ff396c18adb275cebd1cf8ab31b1d847e
Chao Fan <fanchao05@gmail.com>
2018-02-22 16:21:00 -0500

    updated test case

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -125,10 +125,18 @@ describe('DELETE /todos/:id', () => {
   });
 
   it('should return 404 if todo not found', (done) => {
+    const hexId = new ObjectID().toHexString();
 
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
   });
 
   it('should return 404 if object id is invalid', (done) => {
-
+    request(app)
+      .delete('/todos/123')
+      .expect(404)
+      .end(done);
   });
 });
