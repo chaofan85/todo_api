484bef6d26d4ccf2967b24d30ffbd574dbe3701b
Chao Fan <fanchao05@gmail.com>
2018-02-23 15:57:40 -0500

    added token verification

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
-
-const User = mongoose.model('User ', {
+const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
@@ -30,5 +30,22 @@ const User = mongoose.model('User ', {
     }
   }]
 });
+
+UserSchema.methods.generateAuthToken = function() {
+  const user = this;
+  const access = 'auth';
+  const token = jwt.sign({
+    _id: user._id.toHexString(),
+    access
+  }, 'abc123').toString();
+
+  user.tokens = user.tokens.concat([{access, token}]);
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+const User = mongoose.model('User ', UserSchema);
 
 module.exports = {User};
