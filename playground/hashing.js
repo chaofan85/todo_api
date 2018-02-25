97f9e0d73ec96393c729cd45c41ee8e49976d044
Chao Fan <fanchao05@gmail.com>
2018-02-24 19:20:14 -0500

    added password hashing function

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-const data = {
-  id: 10
-};
+const password = '123abc!';
 
-const token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (err1, salt) => {
+  bcrypt.hash(password, salt, (err2, hash) => {
+    console.log(hash);
+  });
+});
 
-const decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+const hashedPassword = '$2a$10$Ie6G2NXDjZwkPP4.TM5MHuB991fLTiklmd6bSV.l/KvMKPcuVSFvi';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// const data = {
+//   id: 10
+// };
+//
+// const token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// const decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 // jwt.verify;
 
 // const message = "I am user number 4";
