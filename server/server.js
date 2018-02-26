7bc5a8b0f7c4508358f3a3f546a653aa99029548
Chao Fan <fanchao05@gmail.com>
2018-02-26 17:08:53 -0500

    updated get todo route

--- server/server.js
+++ server/server.js
@@ -29,8 +29,10 @@ app.post('/todos', authenticate, (req, res) => {
   });
 });
 
-app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+  Todo.find({
+    _creator: req.user._id
+  }).then((todos) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
