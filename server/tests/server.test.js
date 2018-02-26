d560688cf96831274d93f4930e644c2b17cf98f7
Chao Fan <fanchao05@gmail.com>
2018-02-25 22:09:14 -0500

    added login test case

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -220,7 +220,7 @@ describe('POST /users', () => {
           expect(user).toBeTruthy();
           expect(user.password).not.toBe(password);
           done();
-        });
+        }).catch((e) => done(e));
       });
 
   });
@@ -256,5 +256,38 @@ describe('POST /users', () => {
         expect(res.body.email).toBeFalsy();
       })
       .end(done);
+  });
+});
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toBeTruthy();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => done(e));
+      });
+
+  });
+
+  it('should reject invalid login', (done) => {
+
   });
 });
